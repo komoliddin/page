248bc89f8fcde67aaacb3d6e1a0f5141f6b1b5ee
Komoliddin <fuzaylitdinov@gmail.com>
2023-11-02 17:27:33 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,54 @@
+function getDate()
+{
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth();
+    var day = date.getDay();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var milliseconds = date.getMilliseconds();
+    //По надобности условие ниже повторить с minutes и hours
+    if(seconds < 10)
+    {
+        seconds = '0' + seconds;
+    }
+    document.getElementById('timedisplay').innerHTML = year;
+}
+setInterval(getDate, 0);
+
+function myFunction1() {
+    var copyText = document.getElementById("uzcard1");
+    copyText.select();
+    document.execCommand("copy");
+}
+
+function myFunction2() {
+    var copyText = document.getElementById("uzcard2");
+    copyText.select();
+    document.execCommand("copy");
+}
+
+function myFunction3() {
+    var copyText = document.getElementById("yandex1");
+    copyText.select();
+    document.execCommand("copy");
+}
+
+function myFunction4() {
+    var copyText = document.getElementById("btc1");
+    copyText.select();
+    document.execCommand("copy");
+}
+
+function myFunction5() {
+    var copyText = document.getElementById("btc2");
+    copyText.select();
+    document.execCommand("copy");
+}
+
+function myFunction6() {
+    var copyText = document.getElementById("eth1");
+    copyText.select();
+    document.execCommand("copy");
+}
---
+++ vverx/vverx.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    // hide #back-top first
+    $("#back-top").hide();
+
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 200) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 600);
+            return false;
+        });
+    });
+});
